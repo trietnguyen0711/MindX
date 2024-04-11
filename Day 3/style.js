94d9f5ed367cb394d8e5f19932c466f949d8a2ca
trietnguyen0711 <nguyenminhtriet07112008@gmail.com>
2024-04-11 13:30:44 +0700

    DayHomework3

---
+++ Day 3/style.js
@@ -0,0 +1,17 @@
+let a = [`Honda`, `Toyota`, `Kaka`]
+console.log(a)
+a.push(`Nana`)
+console.log(a)
+a.pop()
+console.log(a)
+a.pop([2])
+a.push('hehe')
+
+let nameT = `Minh Triet`
+let age = 16
+let adress = `TPHCM`
+let height = `1m65`
+let inform = [nameT, age, adress]
+let b = `Tôi tên là ${nameT}, năm nay tôi ${age} tuổi, sống ở ${adress} `
+console.log(b)
+inform.push(height)
