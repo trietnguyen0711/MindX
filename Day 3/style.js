f499e0fe109e6393434315f1a2c1184b35d4d7f5
trietnguyen0711 <nguyenminhtriet07112008@gmail.com>
2024-04-12 21:23:10 +0700

    HomeWorkDay3

--- Day 3/style.js
+++ Day 3/style.js
@@ -1,17 +1,31 @@
-let a = [`Honda`, `Toyota`, `Kaka`]
-console.log(a)
-a.push(`Nana`)
-console.log(a)
-a.pop()
-console.log(a)
-a.pop([2])
-a.push('hehe')
-
-let nameT = `Minh Triet`
-let age = 16
-let adress = `TPHCM`
-let height = `1m65`
-let inform = [nameT, age, adress]
-let b = `Tôi tên là ${nameT}, năm nay tôi ${age} tuổi, sống ở ${adress} `
-console.log(b)
-inform.push(height)
+let button = document.querySelectorAll("button");
+let list = document.querySelector(".list");
+let h1 = document.querySelector("h1")
+button[0].addEventListener("click", function () {
+    list.innerHTML = `
+    <button type="button" onclick="functionPrimary()" class="primary btn btn-primary">Primary</button>
+    <button type="button" onclick="functionSecondary()" class="secondary btn btn-secondary">Secondary</button>
+    <button type="button" onclick="functionSuccess()" class="success btn btn-success">Success</button>
+    <button type="button" onclick="functionDanger()" class="danger btn btn-danger">Danger</button>`
+})
+// let primary = document.querySelector("primary")
+// primary.addEventListener("click", function () {
+//     h1.classList.remove("text-primary", "text-secondary", "text-success", "text-danger");
+//     h1.classList.add("text-primary")
+// })
+function functionPrimary() {
+    h1.classList.remove("text-primary", "text-secondary", "text-success", "text-danger");
+    h1.classList.add("text-primary")
+}
+function functionSecondary() {
+    h1.classList.remove("text-primary", "text-secondary", "text-success", "text-danger");
+    h1.classList.add("text-secondary")
+}
+function functionSuccess() {
+    h1.classList.remove("text-primary", "text-secondary", "text-success", "text-danger");
+    h1.classList.add("text-success")
+}
+function functionDanger() {
+    h1.classList.remove("text-primary", "text-secondary", "text-success", "text-danger");
+    h1.classList.add("text-danger")
+}
