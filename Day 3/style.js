bcd86aa08716927b0c2051f9bf400855742dd160
trietnguyen0711 <nguyenminhtriet07112008@gmail.com>
2024-04-13 10:58:21 +0700

    homeWorkDay3

--- Day 3/style.js
+++ Day 3/style.js
@@ -1,31 +1,47 @@
-let button = document.querySelectorAll("button");
+let button = document.querySelector("button");
 let list = document.querySelector(".list");
 let h1 = document.querySelector("h1")
-button[0].addEventListener("click", function () {
+button.addEventListener("click", function () {
     list.innerHTML = `
-    <button type="button" onclick="functionPrimary()" class="primary btn btn-primary">Primary</button>
-    <button type="button" onclick="functionSecondary()" class="secondary btn btn-secondary">Secondary</button>
-    <button type="button" onclick="functionSuccess()" class="success btn btn-success">Success</button>
-    <button type="button" onclick="functionDanger()" class="danger btn btn-danger">Danger</button>`
+    <button type="button" class="primary btn btn-primary">Blue</button>
+    <button type="button" class="secondary btn btn-secondary">Grey</button>
+    <button type="button" class="success btn btn-success">Green</button>
+    <button type="button" class="danger btn btn-danger">Red</button>`
+    alert("Please choose color you want to change")
+    let primary = document.querySelector(".primary")
+    primary.addEventListener("click", function () {
+        h1.classList.remove("text-primary", "text-secondary", "text-success", "text-danger");
+        h1.classList.add("text-primary")
+    })
+    let secondary = document.querySelector(".secondary")
+    secondary.addEventListener("click", function () {
+        h1.classList.remove("text-primary", "text-secondary", "text-success", "text-danger");
+        h1.classList.add("text-secondary")
+    })
+    let success = document.querySelector(".success")
+    success.addEventListener("click", function () {
+        h1.classList.remove("text-primary", "text-secondary", "text-success", "text-danger");
+        h1.classList.add("text-success")
+    })
+    let danger = document.querySelector(".danger")
+    danger.addEventListener("click", function () {
+        h1.classList.remove("text-primary", "text-secondary", "text-success", "text-danger");
+        h1.classList.add("text-danger")
+    })
 })
-// let primary = document.querySelector("primary")
-// primary.addEventListener("click", function () {
+// function functionPrimary() {
 //     h1.classList.remove("text-primary", "text-secondary", "text-success", "text-danger");
 //     h1.classList.add("text-primary")
-// })
-function functionPrimary() {
-    h1.classList.remove("text-primary", "text-secondary", "text-success", "text-danger");
-    h1.classList.add("text-primary")
-}
-function functionSecondary() {
-    h1.classList.remove("text-primary", "text-secondary", "text-success", "text-danger");
-    h1.classList.add("text-secondary")
-}
-function functionSuccess() {
-    h1.classList.remove("text-primary", "text-secondary", "text-success", "text-danger");
-    h1.classList.add("text-success")
-}
-function functionDanger() {
-    h1.classList.remove("text-primary", "text-secondary", "text-success", "text-danger");
-    h1.classList.add("text-danger")
-}
+// }
+// function functionSecondary() {
+//     h1.classList.remove("text-primary", "text-secondary", "text-success", "text-danger");
+//     h1.classList.add("text-secondary")
+// }
+// function functionSuccess() {
+//     h1.classList.remove("text-primary", "text-secondary", "text-success", "text-danger");
+//     h1.classList.add("text-success")
+// }
+// function functionDanger() {
+//     h1.classList.remove("text-primary", "text-secondary", "text-success", "text-danger");
+//     h1.classList.add("text-danger")
+// }
