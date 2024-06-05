b9316780067404743dd46b807be6d60230665d79
trietnguyen0711 <nguyenminhtriet07112008@gmail.com>
2024-06-05 23:35:48 +0700

    SPCK_JSA

--- Day 5/index.js
+++ Day 5/index.js
@@ -5,35 +5,35 @@ if (localStorage.getItem("data")) {
 else {
     let data = [
         {
-            img: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MWND3?wid=400&hei=400&fmt=jpeg&qlt=95&.v=1708125477348",
+            img: "img/sp1.jpg",
             nameProduct: "iPhone 15 Silicone Case with MagSafe - Light Blue",
             price: 49.00,
             id: 0,
             stock: 7
         },
         {
-            img: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MT263?wid=400&hei=400&fmt=jpeg&qlt=95&.v=1699475074951",
+            img: "img/sp2.jpg",
             nameProduct: "iPhone FineWoven Wallet with MagSafe - Pacific Blue",
             price: 49.00,
             id: 1,
             stock: 9
         },
         {
-            img: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MW4Q3?wid=400&hei=400&fmt=jpeg&qlt=95&.v=1707853692193",
+            img: "img/sp3.jpg",
             nameProduct: "45mm Soft Mint Sport Loop very great",
             price: 29.00,
             id: 2,
             stock: 5
         },
         {
-            img: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MWMW3ref?wid=400&hei=400&fmt=jpeg&qlt=95&.v=1707791910829",
+            img: "img/sp4.jpg",
             nameProduct: "45mm Sunshine Sport Band - M/L",
             price: 49.00,
             id: 3,
             stock: 8
         },
         {
-            img: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MT4U3?wid=400&hei=400&fmt=jpeg&qlt=95&.v=1693594248668",
+            img: "img/sp5.jpg",
             nameProduct: "iPhone 15 Pro FineWoven Case - Evergreen",
             price: 59.00,
             id: 4,
@@ -125,18 +125,81 @@ function updateAmount() {
         localStorage.setItem("cart", JSON.stringify(cart))
     }
 }
-let signUp = document.querySelector(".signUp")
-if (localStorage.getItem("user")) {
-    signUp.innerHTML = `<i class="fa-solid fa-user cur-pointer"></i> ${localStorage.getItem("user")}`
-    signUp.addEventListener("click", function () {
-        location.href = "account.html"
+let signUp = document.querySelectorAll(".signUp")
+for (let i = 0; i < signUp.length; i++) {
+    if (localStorage.getItem("user")) {
+        signUp[i].innerHTML = `<i class="fa-solid fa-user cur-pointer"></i> ${localStorage.getItem("user")}`
+        signUp[i].addEventListener("click", function () {
+            location.href = "account.html"
+        })
+    }
+    else {
+        signUp[i].addEventListener("click", function () {
+            location.href = "sign.html"
+        })
+    }
+}
+// Chỉnh light và dark mode
+let header = document.querySelector(".header")
+let aboutUs = document.querySelector(".aboutUs")
+let light = document.querySelectorAll(".light")
+let dark = document.querySelectorAll(".dark")
+let main = document.querySelector(".main")
+let whiteMode = document.querySelectorAll(".whiteMode")
+let grayMode = document.querySelectorAll(".grayMode")
+for (let i = 0; i < light.length; i++) {
+    if (localStorage.getItem("mode")) {
+        let mode = localStorage.getItem("mode")
+        if (mode == "dark") {
+            darkMode()
+        }
+        else {
+            lightMode()
+        }
+    }
+    light[i].addEventListener("click", () => {
+        lightMode()
     })
 }
-else {
-    signUp.addEventListener("click", function () {
-        location.href = "sign.html"
+for (let i = 0; i < dark.length; i++) {
+    dark[i].addEventListener("click", () => {
+        darkMode()
     })
 }
+function lightMode() {
+    header.classList.remove("bg-light")
+    header.classList.add("bg-white")
+    aboutUs.classList.remove("bg-dark")
+    main.classList.remove("bg-dark")
+    main.classList.add("bg-light")
+    for (let i = 0; i < whiteMode.length; i++) {
+        whiteMode[i].classList.remove("color-white")
+    }
+    for (let i = 0; i < grayMode.length; i++) {
+        grayMode[i].classList.remove("color-gray")
+    }
+    localStorage.setItem("mode", "light")
+}
+function darkMode() {
+    header.classList.add("bg-light")
+    header.classList.remove("bg-white")
+    aboutUs.classList.add("bg-dark")
+    main.classList.remove("bg-light")
+    main.classList.add("bg-dark")
+    for (let i = 0; i < whiteMode.length; i++) {
+        whiteMode[i].classList.add("color-white")
+    }
+    for (let i = 0; i < grayMode.length; i++) {
+        grayMode[i].classList.add("color-gray")
+    }
+    localStorage.setItem("mode", "dark")
+}
+// Hien ds header
+let listChoose = document.querySelector(".listChoose")
+let ds = document.querySelector(".ds")
+listChoose.addEventListener("click", () => {
+    ds.classList.remove("d-none")
+})
 // Các chức năng cần bổ sung vào trang web
 // - Confirm mật khẩu đăng kí (COMPLETED)
 // - Trừ số lượng hàng sau khi đặt hàng thành công
